cb4b75d7e1a80a285c7101165d28dc17fa9f250f
Varshitha1895 <vvarshi317@gmail.com>
2026-04-07 22:58:00 +0530

    Add reset function to inference.py

---
+++ inference.py
@@ -0,0 +1,8 @@
+import server.inference as inference
+
+def reset():
+    return inference.reset()
+
+if __name__ == "__main__":
+    # Meta validator check chesthe idi success chupisthundi
+    print(reset())
