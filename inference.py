9cdf8c97f6edce3c3dbd69e90f1f3e7798b3970a
Varshitha1895 <vvarshi317@gmail.com>
2026-04-08 23:02:47 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,8 +1,4 @@
 import sys
 import os
-
-# Server folder path ni Python ki chepthunnam
 sys.path.append(os.path.join(os.path.dirname(__file__), 'server'))
-
-# Ikkada nundi server folder lo unna original code ni pilusthundhi
 from server.inference import *
